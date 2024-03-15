494af007fa21b64cc39468027963d1eac0f88c06
Mohammad Jafari <66032533+mhdjafari@users.noreply.github.com>
2024-03-14 21:16:53 -0700

    Add Google Analytics

--- Fintekera/app/(site)/layout.tsx
+++ Fintekera/app/(site)/layout.tsx
@@ -40,6 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`dark:bg-black ${inter.className}`}>
+
        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
         <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
       ) : null}
